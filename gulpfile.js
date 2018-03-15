0e817a9a46f616fea35d51ae62120e601b5dd1ad
Tushar Sachde <tushar.sachde@sptr.co>
2018-03-15 15:44:59 +0530

    Fixing gulpfile.js
    Co-Authored-By: sohan1 <shn619@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,31 @@
 var jsArray = [
 
     // Config file
-    './js/misc/detector.js',
     './js/config.js',
 
     // JS & jQuery Libs
-    './bower_components/jquery/dist/jquery.min.js',
-    './bower_components/jquery-mousewheel/jquery.mousewheel.js',
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/bootstrap/dist/js/bootstrap.min.js',
     './bower_components/jStorage/jstorage.js',
-    './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-    './bower_components/flexslider/jquery.flexslider-min.js',
-    './bower_components/fancyBox/source/jquery.fancybox.js',
-    './bower_components/smart-app-banner/smart-app-banner.js',
     './bower_components/moment/min/moment.min.js',
-    './bower_components/jquery.transit/jquery.transit.js',
+    './bower_components/font-awesome/svg-with-js/js/fontawesome-all.js',
+    './bower_components/fancybox/dist/jquery.fancybox.js',
 
     // Angular Libs
     './bower_components/angular/angular.js',
     './bower_components/angular-sanitize/angular-sanitize.js',
     './bower_components/angular-animate/angular-animate.min.js',
-    './bower_components/angular-bootstrap/ui-bootstrap.min.js',
-    './bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+    './bower_components/angular-touch/angular-touch.min.js',
     './bower_components/angular-ui-router/release/angular-ui-router.min.js',
-    './bower_components/angular-flexslider/angular-flexslider.js',
+    './bower_components/ui-bootstrap4/docs/ui-bootstrap.js',
+    './bower_components/ui-bootstrap4/docs/ui-bootstrap-tpls.js',
     './bower_components/angular-translate/angular-translate.js',
-    './bower_components/lodash/lodash.js',
     './bower_components/angulartics/dist/angulartics.min.js',
     './bower_components/angulartics-google-analytics/dist/angulartics-ga.min.js',
     './bower_components/angular-ui-swiper/dist/angular-ui-swiper.js',
     './bower_components/ngToast/dist/ngToast.js',
     './bower_components/angular-socialshare/dist/angular-socialshare.js',
-    './bower_components/angular-fullscreen/src/angular-fullscreen.js',
+    './bower_components/lodash/lodash.js',
 
     // Misc
     './js/misc/animate.css.js',
@@ -45,7 +40,6 @@ var jsArray = [
     // Directives
     './js/directives/directive.js',
     './js/directives/brightcove-player.js',
-    './js/directives/aframe.js',
 
     // Filters
     './js/filters/filter.js',
